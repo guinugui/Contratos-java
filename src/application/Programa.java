0d111a4f90dd0693a951b12c55e4ae83a5bbdb84
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-03-02 10:13:54 -0300

    Classe hora por contrato concluida

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,19 +1,27 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Departamento;
+import entities.horaPorContrato;
 
 public class Programa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Departamento depat = new Departamento("Desing");
-		System.out.println("Nome; " + depat.getNome());
+		Date n = sdf.parse("24/03/2002");
+		
+		horaPorContrato hor = new horaPorContrato(n, 5.0, 5);
+		
+		System.out.println(hor.valorTotal());
+		
 		
 		
 		
--- src/entities/horaPorContrato.java
+++ src/entities/horaPorContrato.java
@@ -1,5 +1,45 @@
 package entities;
 
+import java.util.Date;
+
 public class horaPorContrato {
+
+	private Date data;
+	private Double valorPorHoras;
+	private Integer horas;
+
+	public horaPorContrato(Date data, Double valorPorHoras, Integer horas) {
+		this.data = data;
+		this.valorPorHoras = valorPorHoras;
+		this.horas = horas;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Double getValorPorHoras() {
+		return valorPorHoras;
+	}
+
+	public void setValorPorHoras(Double valorPorHoras) {
+		this.valorPorHoras = valorPorHoras;
+	}
+
+	public Integer getHoras() {
+		return horas;
+	}
+
+	public void setHoras(Integer horas) {
+		this.horas = horas;
+	}
+
+	public Double valorTotal() {
+		return horas * valorPorHoras;
+	}
 
 }

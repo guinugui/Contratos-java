2f2c0e8b65655c5f8797fd5287821ad2454cabed
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-03-02 10:47:56 -0300

    Classe funcionario concluida

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,8 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.horaPorContrato;
 
 public class Programa {
 
@@ -14,13 +10,7 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Date n = sdf.parse("24/03/2002");
-		
-		horaPorContrato hor = new horaPorContrato(n, 5.0, 5);
-		
-		System.out.println(hor.valorTotal());
+	
 		
 		
 		
--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -1,5 +1,93 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.nivelDoFuncionario;
+
 public class Funcionario {
 
+	private String nome;
+	private nivelDoFuncionario nivel;
+	private Double salarioBase;
+	
+	
+	private Departamento departamento;
+	private List<horaPorContrato> contratos = new ArrayList<>();
+
+	public Funcionario(String nome, nivelDoFuncionario nivel, Double salarioBase, Departamento departamento) {
+		this.nome = nome;
+		this.nivel = nivel;
+		this.salarioBase = salarioBase;
+		this.departamento = departamento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public nivelDoFuncionario getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(nivelDoFuncionario nivel) {
+		this.nivel = nivel;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<horaPorContrato> getContratos() {
+		return contratos;
+	}
+
+	public void setContratos(List<horaPorContrato> contratos) {
+		this.contratos = contratos;
+	}
+	
+	
+	public void addContratos(horaPorContrato contrato) {
+		contratos.add(contrato);
+	}
+	public void removeContratos(horaPorContrato contrato) {
+		contratos.remove(contrato);
+	}
+
+	public Double renda(Integer ano, Integer mes) {
+		double sum = salarioBase;
+		Calendar cal = Calendar.getInstance();
+		for(horaPorContrato c : contratos) {
+			cal.setTime(c.getData());
+			int ANO = cal.get(Calendar.YEAR);
+			int MES =1 + cal.get(Calendar.MONTH);
+			if(ANO == ano && MES == mes) {
+				sum += c.valorTotal();
+			}
+		}
+		return sum;
+	}
+	
+	
+	
+	
+	
+	
 }
